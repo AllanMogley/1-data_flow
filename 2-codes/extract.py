4ca31d768b531a262417506b6d759be1f6bca7b3
allan_mogley_debian <allanmogley@gmail.com>
2024-11-13 22:36:33 +0300

    (Update) : Refined Exrtractoy .py code

--- 2-codes/extract.py
+++ 2-codes/extract.py
@@ -13,11 +13,11 @@ gis=GIS()
 # Access a hosted Data Source and Query USA data
 src_url = os.getenv('src_url')
 fl = Service(src_url)
-fset_usa = fl.query(where=os.getenv('country'))
-fset_usa.sdf.to_csv(os.getenv('covid_data_out'))
+fset_usa = fl.query(where="Country_Region='US'")
+fset_usa.sdf.to_csv(os.getenv('covid_data'))
 
 # Get USA Country level Boundaries
 us_states_item = gis.content.get(os.getenv('content'))
 us_states_flayer = us_states_item.layers[0]
 us_states_df = us_states_flayer.query(as_df=True)
-us_states_df.to_csv(os.getenv('country_data_out'))
+us_states_df.to_csv(os.getenv('usa_bounds'))
